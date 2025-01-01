666f6f2ba580142898f8b8db41023005bbbd0eee
Codanvanced <50668308+Codanvanced@users.noreply.github.com>
2025-01-01 21:41:58 +0000

    Primer commit

---
+++ Cloud/w-next-docker.ps1
@@ -0,0 +1,19 @@
+# Montar el volumen
+docker volume create `
+--driver local `
+--name nextcloud_aio_nextcloud_datadir `
+-o device="W:/NextCloud" `
+-o type="none" `
+-o o="bind"
+
+# Arrancar el contenedor.
+docker run `
+--sig-proxy=false `
+--name nextcloud-aio-mastercontainer `
+--publish 80:80 `
+--publish 8080:8080 `
+--publish 8443:8443 `
+--volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config `
+--volume //var/run/docker.sock:/var/run/docker.sock:ro `
+-e NEXTCLOUD_DATADIR="nextcloud_aio_nextcloud_datadir" `
+nextcloud/all-in-one:latest
